eaad540be9af2de05913955ca8902db9b4c203be
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-07-22 17:15:44 -0400

    created static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+
+  resources :admins
+
+  resources :pages
+    get 'about', to: 'pages#about', as: 'about'
+
   resources :sessions, only: [:new, :create, :destroy]
     get 'signup', to: 'admins#new', as: 'signup'
     get 'login', to: 'sessions#new', as: 'login'
     get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  resources :admins
-
-  get 'welcome/index'
-  root 'welcome#index'
+    get 'welcome/index'
+    root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
