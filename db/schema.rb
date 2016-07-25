072ff2f9eac1e00feaa4611a6014b1ed8117dc40
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-07-25 19:56:14 -0400

    added body column to blogs and projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725234953) do
+ActiveRecord::Schema.define(version: 20160725235407) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "password"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160725234953) do
     t.integer  "admin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
   create_table "pictures", force: :cascade do |t|
@@ -35,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160725234953) do
     t.integer  "admin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
