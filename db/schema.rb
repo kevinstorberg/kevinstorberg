7e9f74e46463448f662d256597e7ba1c0180f00c
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-07-27 00:00:12 -0400

    added titles to projects and blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727035544) do
+ActiveRecord::Schema.define(version: 20160727035806) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "password"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160727035544) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "body"
+    t.string   "title"
   end
 
   create_table "pictures", force: :cascade do |t|
@@ -39,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160727035544) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "body"
+    t.string   "title"
   end
 
 end
