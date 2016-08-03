9a4a634c6f8cf504091257991f7a9e20a6984aba
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-08-03 17:55:23 -0400

    added validations to admins, projects, and blogs

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,6 @@ class Admin < ApplicationRecord
   has_secure_password
   has_many :blogs, dependent: :destroy
   has_many :projects, dependent: :destroy
+  validates :name, :inclusion => {:in => %w(kevinstorberg)}
+  validates :name, :uniqueness => {:case_sensitive => true}
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
   has_one :picture, as: :pictureable, dependent: :destroy
   accepts_nested_attributes_for :picture
+  validates :body, :presence => true
+  validates :title, :presence => true
+  validates_associated :picture, :presence => true
 end
